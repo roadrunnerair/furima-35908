3c24b4bf3bee2e0815fb9df0ce15f9ed25b5fe14
roadrunnerair <machine.x240@gmail.com>
2021-09-01 22:12:42 +0900

    バリデーションの設定 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  validates :email,              presence: true, uniqueness: true
-  validates :password,           presence: true, format: { with: /\A[a-z0-9]+\z/i }
-  validates :encrypted_password, presence: true, format: { with: /\A[a-z0-9]+\z/i }
+  validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_kana,         presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
