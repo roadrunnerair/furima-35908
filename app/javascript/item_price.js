099531034dc570e45afe90378be7b7d37d79691b
roadrunnerair <machine.x240@gmail.com>
2021-09-08 21:20:07 +0900

    必要な要素を取得する

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,5 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price");
+  console.log(priceInput);
+})
