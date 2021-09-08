01e828ff626d4ba066b6386d1a00d80da58a5626
roadrunnerair <machine.x240@gmail.com>
2021-09-08 12:50:55 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    # @item = Item.all
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
