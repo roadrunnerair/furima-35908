48d6086f7c4879ad1aa09a80ced27e64cbb68f53
roadrunnerair <machine.x240@gmail.com>
2021-09-09 22:44:53 +0900

    エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,12 @@ class Item < ApplicationRecord
   validates :image,                       presence: true
   validates :name,                        presence: true, length: { maximum: 40 }
   validates :item_description,            presence: true, length: { maximum: 1000 }
-  validates :category_id,                 presence: true
-  validates :item_condition_id,           presence: true
-  validates :item_postage_id,             presence: true
-  validates :prefecture_id,               presence: true
-  validates :estimated_shipping_date_id,  presence: true
-  validates :price,                       presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :price,                       numericality: { only_integer: true }
+  validates :category_id,                 numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_condition_id,           numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_postage_id,             numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,               numericality: { other_than: 1, message: "can't be blank" }
+  validates :estimated_shipping_date_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,                       numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,                       numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,                       numericality: { other_than: 1, message: 'is invalid. Input half-width characters.' }
 end
