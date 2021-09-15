d645b42b06afb24857112f64b617fdc01ee53b54
roadrunnerair <machine.x240@gmail.com>
2021-09-15 21:44:53 +0900

    rubocopの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit,:update]
 
   def index
     @item = Item.all.order(created_at: :DESC)
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :estimated_shipping_date
 
-
   with_options presence: true do
     validates :image
     validates :name,              length: { maximum: 40 }
     validates :item_description,  length: { maximum: 1000 }
-    validates :price, numericality: { message: 'is invalid. Input half-width characters.', 
-      only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { message: 'is invalid. Input half-width characters.',
+                                      only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   with_options numericality: { other_than: 0, message: "can't be blank" } do
