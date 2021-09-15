1aa11f0e3e1c5b29f8ee10040315a8422fea4cf0
roadrunnerair <machine.x240@gmail.com>
2021-09-15 21:42:03 +0900

    updateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @item = Item.all.order(created_at: :DESC)
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -18,21 +22,31 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # end
-
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
-  def show
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
+
+  # def destroy
+  # end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :price, :item_description, :category_id, :item_condition_id, :item_postage_id,
                                  :prefecture_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
