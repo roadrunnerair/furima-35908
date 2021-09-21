65574b71012cae6d85f7398ac2d27497d771a4cb
roadrunnerair <machine.x240@gmail.com>
2021-09-21 15:49:16 +0900

    destroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # end
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
+  end
 
   private
 
