7345136263f5b4532b0acb1f12f07539122d181a
roadrunnerair <machine.x240@gmail.com>
2021-10-03 22:44:52 +0900

    不必要な記述の消去

--- db/schema.rb
+++ db/schema.rb
@@ -33,15 +33,6 @@ ActiveRecord::Schema.define(version: 2021_09_30_094411) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "card_token", null: false
-    t.string "customer_token", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
@@ -98,7 +89,6 @@ ActiveRecord::Schema.define(version: 2021_09_30_094411) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "cards", "users"
   add_foreign_key "items", "users"
   add_foreign_key "mailing_addresses", "order_histories"
   add_foreign_key "order_histories", "items"
